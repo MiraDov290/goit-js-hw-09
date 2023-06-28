9aebd527da868a75581ef0e3c54ff71276773697
MiraDov290 <muzhyk.myroslava@gmail.com>
2023-06-28 11:35:16 +0100

    changers

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+import getRandomHexColor from './randomHexColor.js';
+
+
+const dataStart = document.querySelector('[data-start]');
+const dataStop = document.querySelector('[data-stop]');
+const bodyColor = document.querySelector('body');
+let timerId = null;
+
+
+dataStart.addEventListener('click', onStart);
+dataStop.addEventListener('click', onStop);
+
+
+function onStart() {
+timerId = setInterval(getBgColor, 1000);
+
+dataStart.toggleAttribute('disabled');
+}
+
+
+function onStop() {
+clearInterval(timerId);
+
+dataStart.removeAttribute('disabled');
+}
+
+
+function getBgColor() {
+bodyColor.style.backgroundColor = getRandomHexColor();
+}
+
+
+
 
